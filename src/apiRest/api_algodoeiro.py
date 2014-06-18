920f34fad122a54490355e0259c0e7985b5456b4
Celio Roberto Cavalcante de Barros Filho <celio@pearson.(none)>
2014-06-18 15:56:40 -0300

    Api Rest refatorada

--- src/apiRest/api_algodoeiro.py
+++ src/apiRest/api_algodoeiro.py
@@ -3,30 +3,9 @@ import dadosApiRest
 
 app = Flask(__name__)
 
-@app.route('/produtividade_regiao2')
-def produtividade_regiaoJson():
-	response = dados.produtividade_regiaoJson()
-	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
-	return response
-
 @app.route('/regioes')
 def regiao():
 	response = dadosApiRest.regiao()
-	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
-	return response
-
-@app.route('/comunidades')
-def comunidade():
-	response = dadosApiRest.comunidade()
-	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
-	return response
-
-@app.route('/culturas')
-def cultura():
-	response = dadosApiRest.cultura()
 	response = make_response(response)
 	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
@@ -49,13 +28,6 @@ def media_producao_regiao():
 @app.route('/produtividade_dos_agricultores')
 def produtividade_agricultores():
 	response = dadosApiRest.produtividade_agricultores()
-	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
-	return response
-
-@app.route('/produtividade_agricultor/<int:id_agricultor>')
-def produtividade_agricultor(id_agricultor):
-	response = dadosApiRest.produtividade_agricultor(id_agricultor)
 	response = make_response(response)
 	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
--- src/apiRest/dadosApiRest.py
+++ src/apiRest/dadosApiRest.py
@@ -21,30 +21,6 @@ def regiao():
     col = ["id", "regiao"]
     return montaJson(montaListaJson(lista_tuplas, col))
 
-def comunidade():
-    cnxn = create_connection()
-    cursor = cnxn.cursor()
-    cursor.execute("select * from Comunidade")
-    rows = cursor.fetchall()
-    cnxn.close()
-    lista_tuplas = []
-    for tupla in rows:
-       lista_tuplas.append(tupla)
-    col = ["id", "comunidade", "cidade", "regiao"]
-    return montaJson(montaListaJson(lista_tuplas, col))
-
-def cultura():
-    cnxn = create_connection()
-    cursor = cnxn.cursor()
-    cursor.execute("select * from Cultura")
-    rows = cursor.fetchall()
-    cnxn.close()
-    lista_tuplas = []
-    for tupla in rows:
-       lista_tuplas.append(tupla)
-    col = ["id", "nome_cultura"]
-    return montaJson(montaListaJson(lista_tuplas, col))
-
 def agricultores():
     cnxn = create_connection()
     cursor = cnxn.cursor()
@@ -80,33 +56,6 @@ def produtividade_agricultores():
        lista_tuplas.append(tupla)
     col = ["id_agricultor", "nome_cultura","id_cultura","producao"]
     return montaJson(montaListaJson(lista_tuplas, col))
-
-def produtividade_agricultor(id_agricultor):
-    cnxn = create_connection()
-    cursor = cnxn.cursor()
-	# visualizacao da produtividade de um agricultor. exibidas as seguintes informacoes no grafico:
-	# area total de plantio de cada cultura, as quantidades produzidas, o nome das culturas, data plantio
-	# TESTE a.nome_agricultor, p.area_plantada, , p.data_plantio  and year(p.data_plantio)=2010
-    cursor.execute("select c.nome_cultura, p.quantidade_produzida from Producao p, Cultura c where p.id_agricultor=%d and c.id=p.id_cultura" %(id_agricultor))
-    rows = cursor.fetchall()
-    cnxn.close()
-    lista_tuplas = []
-    for tupla in rows:
-       lista_tuplas.append(tupla)
-
-    lista_tupla_tupla = collections.defaultdict(list)
-    for item in lista_tuplas:
-        lista_tupla_tupla[item[0]].append(item)
-    # find sum of values
-    lista_tuplas = []
-    for key, value in lista_tupla_tupla.items():
-        lista_final = [key] + map(sum, zip(*value)[1:])
-	lista_final = tuple(lista_final)
-        lista_tuplas.append(lista_final)
- 
-    col = ["nome_cultura", "quantidade_produzida"]
-    newJson = '{"name": "agricultor","children":'+ montaJson(montaListaJson(lista_tuplas, col)) +'}'
-    return newJson
 
 def produtividade_regiao():
     cnxn = create_connection()
