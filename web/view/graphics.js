cc803e2150651eb3c39fe09892806c71ecb70b26
caynan <caynanvls@gmail.com>
2014-06-25 19:19:21 -0300

    Formatado estrutura da area do dropdown, utilizando grid layout(bootstrap), adicionado informações sobre o agricultor (cidade, comunidade e area plantada)

--- web/view/graphics.js
+++ web/view/graphics.js
@@ -183,68 +183,143 @@ function graph3() {
     });
 
     //DropDown regiões
-    var selectRegioes = d3.select("#droplist_regioes").append("select").attr("id", "select_regioes").on("change", function() {
-        changeAgricultores(this.options[this.selectedIndex].value);
-    }).selectAll("option").data(regioes).enter().append("option").attr("value", function(d) {
-        return d.id;
-    }).text(function(d) {
-        return d.regiao;
-    });
+    var selectRegioes = d3.select("#droplist_regioes")
+        .append("select")
+        .attr("id", "select_regioes")
+        .on("change", function() {
+                changeAgricultores(this.options[this.selectedIndex].value);
+            }
+        )
+        .selectAll("option")
+            .data(regioes)
+            .enter()
+            .append("option")
+            .attr("value", function(d) {
+            return d.id;
+        })
+        .text(function(d) {
+            return d.regiao;
+        });
 
     //DropDown agricultores
     var selectAgricultores = d3.select("#droplist_agricultores").append("select").attr("id", "select_agricultores").on("change", function() {
         changeGraficoProduAgricultor(this.options[this.selectedIndex].value, $("#select_regioes").val());
-    }).selectAll("option").data(agricultores).enter().append("option").attr("value", function(d) {
+    })
+    .selectAll("option")
+        .data(agricultores)
+        .enter()
+        .append("option")
+        .attr("value", function(d) {
         return d.id;
-    }).text(function(d) {
+    })
+    .text(function(d) {
         return d.nome_agricultor;
     });
 
     function changeAgricultores(regiaoSelecionadaId) {
-        //Remove agricultores do dropdown
+        // Remove agricultores do dropdown
         d3.select("#select_agricultores").selectAll("option").remove();
 
-        //Seleciona agricultores
+        // Seleciona agricultores
         var agricultoresDaRegiao = _.filter(agricultores, function(agricultor) {
             return regiaoSelecionadaId == agricultor.id_regiao;
         });
 
-        //Popula DropDown
-        d3.select("#select_agricultores").selectAll("option").data(agricultoresDaRegiao).enter().append("option").attr("value", function(d) {
-            return d.id;
-        }).text(function(d) {
-            return d.nome_agricultor;
-        });
+        // Popula DropDown
+        d3.select("#select_agricultores")
+            .selectAll("option")
+            .data(agricultoresDaRegiao)
+            .enter()
+            .append("option")
+            .attr("value", function(d) {
+                return d.id;
+            })
+            .text(function(d) {
+                return d.nome_agricultor;
+            });
 
-        //Valor Default
+        // Valor Default
         var valorAtualAgricultores = $("#select_agricultores").val();
+
         changeGraficoProduAgricultor(valorAtualAgricultores, regiaoSelecionadaId);
     }
 
+    function dropAllInfos() {
+        d3.select("#info_comunidade").selectAll("g").remove();
+        d3.select("#info_cidade").selectAll("g").remove();
+        d3.select("#info_area_produzida").selectAll("g").remove();
+    }
+
+    function changeInfoAgricultor(agricultorId, regiaoSelecionadaId) {
+        // remove dados que ja existam
+        dropAllInfos();
+
+        var agricultorSelecionado = _.filter(agricultores, function(object) {
+            return object.id == agricultorId;
+        })[0];
+
+        var producaoSelecionada = _.filter(produ_agricultores, function(object) {
+            return object.id_agricultor == agricultorId;
+        });
+        
+        var comunidadeMsg = agricultorSelecionado.nome_comunidade;
+
+        // Precisa atualizar API REST
+        // var cidadeMsg = agricultorSelecionado.nome_cidade;
+
+        var areaValue = producaoSelecionada[0].area;
+        // Testa para valores null
+        if (areaValue !== null) {
+            areaMsg = areaValue + " ha";
+        } else {
+            areaMsg = "Area Produzida Não Informada";
+        }
+       
+        // append nome comunidade
+        d3.select("#info_comunidade")
+            .append("g")
+            .text(comunidadeMsg);
+
+        // API REST PRECISA SER ATUALIZADA
+        // // append nome cidade
+        // d3.select('#info_cidade')
+        //     .append("g")
+        //     .text(cidadeMsg);
+
+        // append area produzida
+        d3.select('#info_area_produzida')
+            .append("g")
+            .text(areaMsg);
+    }
+
     function changeGraficoProduAgricultor(agricultorId, regiaoSelecionadaId) {
-        //Produção do Agricultor
+        // Produção do Agricultor
         var selecionados = _.filter(produ_agricultores, function(object) {
             return object.id_agricultor == agricultorId;
         });
+
         var producaoAgricultor = selecionados;
         producaoAgricultor = _.sortBy(producaoAgricultor, "id_cultura");
 
-        //Média região atual
+        // Média região atual
         var media_regiao_atual = _.filter(media_producao_regiao, function(object) {
             return object.id_regiao == regiaoSelecionadaId;
         });
-        //Média da produção da região das culturas que o agricultor plantou
+        
+        // Média da produção da região das culturas que o agricultor plantou
         var media_agricultor = _.filter(media_regiao_atual, function(object) {
             return _.contains(_.pluck(selecionados, 'id_cultura'), object.id_cultura);
         });
         media_agricultor = _.sortBy(media_agricultor, "id_cultura");
 
-        //Cria dataframe
+        // Cria dataframe
         var quant_culturas = media_agricultor.length;
         var layers = [producaoAgricultor, media_agricultor];
         var labels = _.pluck(selecionados, 'nome_cultura');
-
+        
+        changeInfoAgricultor(agricultorId, regiaoSelecionadaId);
         generateBarGraph(layers, labels);
+
     }
 
     function generateBarGraph(layers, labels) {
@@ -320,19 +395,6 @@ function graph3() {
         legend.append("rect").attr("x", width - 2).attr("y", 30).attr("width", 10).attr("height", 10).style("fill", color);
         legend.append("text").attr("x", width - 6).attr("y", 35).attr("dy", ".35em").style("text-anchor", "end").text(function(d) {
             return d;
-        });
-
-        // Area
-        var areaValue = layers[0][0].area;
-        var area = svg.append("g").attr("id", "area_plantada");
-        console.log(areaValue);
-
-        area.append("text").attr("x", width - 6).style("text-anchor", "end").text(function(d) {
-            if (areaValue !== null) {
-                return "Area Produzida: " + areaValue + " ha";
-            } else {
-                return "Area Produzida Não Informada";
-            }
         });
     }
 
