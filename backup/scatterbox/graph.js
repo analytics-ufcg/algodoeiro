dcb794463999d67f1536a777c0ef64d350a4948c
talitabac <talitabac@gmail.com>
2014-06-28 11:50:32 -0300

    estudos de d3

--- backup/scatterbox/graph.js
+++ backup/scatterbox/graph.js
@@ -1,48 +1,64 @@
-	
-	var	width = 420;
-	var	barHeight = 20;
-	
-	var x = d3.scale.linear()
-		.range([0, width]);
 		
-	var chart = d3.select(".chart")
-		.attr("width", width);
-		
+	var margin = {top: 20, right: 20, bottom: 30, left: 40},
+	    width = 960 - margin.left - margin.right,
+	    height = 500 - margin.top - margin.bottom;
+	
+	var x = d3.scale.ordinal()
+	    .rangeRoundBands([0, width], .1);
+	
+	var y = d3.scale.linear()
+	    .range([height, 0]);
+	
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+	
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left")
+	    .ticks(10, "%");
+	
+	var svg = d3.select("body").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 	
 	d3.tsv("data.tsv", type, function(error, data) {
-		x.domain([0, d3.max(data, function(d) {
-			return d.value;
-		})]);
-		
-		chart.attr("height", barHeight * data.length);
-		
-		var bar = chart.selectAll("g")// nao entendi esse g
-			.data(data).enter().append("g")// nao entendi esse append
-			.attr("transform", function(d, i) {
-				return "translate(0," + i * barHeight + ")"; // ver funcion d, i
-		});
-		
-		bar.append("rect")
-		.attr("width", x)
-		.attr("height", barHeight - 1);
-		
-		bar.append("text")
-		.attr("x", function(d) {
-			return x(d) - 3;
-		})
-		.attr("y", barHeight / 2)
-		.attr("dy", ".35em")
-		.text(function(d) {
-			return d;
-		});	
-		
+	  x.domain(data.map(function(d) { return d.letter; }));
+	  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+	
+	  svg.append("g")
+	      .attr("class", "x axis")
+	      .attr("transform", "translate(0," + height + ")")
+	      .call(xAxis);
+	
+	  svg.append("g")
+	      .attr("class", "y axis")
+	      .call(yAxis)
+	    .append("text")
+	      .attr("transform", "rotate(-90)")
+	      .attr("y", 6)
+	      .attr("dy", ".71em")
+	      .style("text-anchor", "end")
+	      .text("Frequency");
+	
+	  svg.selectAll(".bar")
+	      .data(data)
+	    .enter().append("rect")
+	      .attr("class", "bar")
+	      .attr("x", function(d) { return x(d.letter); })
+	      .attr("width", x.rangeBand())
+	      .attr("y", function(d) { return y(d.frequency); })
+	      .attr("height", function(d) { return height - y(d.frequency); });
+	
 	});
 	
 	function type(d) {
-		d.value = d+d.value;
-		return d;
+	  d.frequency = +d.frequency;
+	  return d;
 	}
-	
+
 	
 	
 	
