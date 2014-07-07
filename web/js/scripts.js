64ca54801bafc5e1dd92314053ba1569ac52b159
talitalobo <talitabac@gmail.com>
2014-07-07 11:04:06 -0300

    visibilidade divs

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,15 +1,8 @@
+
 function toogleVisib(id) {
-	var listaelementos = document.getElementsByClassName("listalinks");
-
-	for (var i = 0; i < listaelementos.lenght; i++) {
-		listaelementos[i].style.display = 'none';
-	}
-
-	var toShow = document.getElementById(id);
-	if (toShow.style.display == 'none') {
-		toShow.style.display == 'block';
-		console.log("mudou o estilo do elemento");
-	} else {
-		console.log("ja ta sendo mostrado");
-	}
+	var listaelementos = $(".listalinks");
+	
+	listaelementos.hide();
+	var toShow = $("#" + id);
+	toShow.show();
 }
