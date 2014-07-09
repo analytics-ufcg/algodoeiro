6e133008839304e4a45cf7ae3f8ca02940cdc1dc
Celio Roberto Cavalcante de Barros Filho <celio@spearman.(none)>
2014-07-09 18:17:29 -0300

    Scripts das tecnicas adotadas

--- src/SQL/copy.sql
+++ src/SQL/copy.sql
@@ -3,3 +3,6 @@ COPY Producao(id_agricultor,id_cultura, area_plantada, quantidade_produzida, dat
 COPY Atividade(atividade, unidade) FROM LOCAL '/home/celio/Desenvolvimento/algodoeiro/src/dados/Atividades.csv' WITH DELIMITER ',' SKIP 1;
 
 COPY Custo(id_atividade, id_regiao,quantidade,valor_unitario, area) FROM LOCAL '/home/celio/Desenvolvimento/algodoeiro/src/dados/Custos.csv' WITH DELIMITER ',';
+
+COPY Tecnica_Adotada(id_agricultor,ano,id_tecnica) FROM LOCAL '/home/celio/Desenvolvimento/algodoeiro/src/dados/Tecnicas_adotadas.csv' WITH DELIMITER ',';
+
