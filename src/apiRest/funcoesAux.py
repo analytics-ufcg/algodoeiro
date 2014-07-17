5b20190058ca49f036bfb74426f034d6ba2705b1
Celio Roberto Cavalcante de Barros Filho <celio@pearson.(none)>
2014-07-17 14:42:48 -0300

    Refatorando apiRest, separando-a em mais de uma para ficar mais facil manter

---
+++ src/apiRest/funcoesAux.py
@@ -0,0 +1,19 @@
+import json
+import collections
+import operator
+
+def montaListaJson(spamreader, col):
+	response = []
+	colunas = col
+	for row in spamreader:
+		celulas = {}
+		for indexColumns in range(0,len(colunas)):
+			celulas[colunas[indexColumns]] = row[indexColumns]
+		response.append(celulas)
+	return response
+
+def montaJson(response,sorted = False):
+	return json.dumps(response,sort_keys=sorted,default=date_handler).encode('utf8')
+
+def date_handler(obj):
+    return obj.isoformat() if hasattr(obj, 'isoformat') else obj
