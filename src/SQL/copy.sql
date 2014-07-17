451dbb7d2ca5bc86a1c7f791a0b7007453497daf
Celio Roberto Cavalcante de Barros Filho <celio@pearson.(none)>
2014-07-17 10:25:38 -0300

    Tabela das certificacoes e tabela das certificacoes com agricultor

--- src/SQL/copy.sql
+++ src/SQL/copy.sql
@@ -6,9 +6,10 @@ COPY Custo(id_atividade, id_regiao,quantidade,valor_unitario, area) FROM LOCAL '
 
 COPY Tecnica_Adotada(id_agricultor,ano,id_tecnica) FROM LOCAL '/home/celio/Desenvolvimento/algodoeiro/src/dados/Tecnicas_adotadas.csv' WITH DELIMITER ',';
 
-
-DROP TABLE Producao;
-
-CREATE TABLE Producao(id auto_increment, id_agricultor INT, id_cultura INT, area_plantada FLOAT, quantidade_produzida FLOAT, data_plantio DATE, PRIMARY KEY (id), FOREIGN KEY (id_agricultor) REFERENCES Agricultor,  FOREIGN KEY (id_cultura) REFERENCES Cultura (id));
-
 COPY Producao(id_agricultor,id_cultura, area_plantada, quantidade_produzida, data_plantio format 'DD/MM/YYYY') FROM LOCAL '/home/andryw/algodoeiro/src/dados/TABELA_PRODUCAO.csv' WITH DELIMITER ',';
+
+COPY Ano(ano_producao) FROM LOCAL '/home/celio/Desenvolvimento/algodoeiro/src/dados/Anos.csv' WITH DELIMITER ',' SKIP 1;
+
+COPY Certificacao(nome_certificacao, nome_simplificado_certificacao) FROM LOCAL '/home/celio/Desenvolvimento/algodoeiro/src/dados/Certificacao.csv' WITH DELIMITER ',' SKIP 1;
+
+COPY Agricultor_Certificacao(id_agricultor,ano_producao,id_certificacao) FROM LOCAL '/home/celio/Desenvolvimento/algodoeiro/src/dados/Agricultores_com_Certificacao.csv' WITH DELIMITER ',' SKIP 1;
