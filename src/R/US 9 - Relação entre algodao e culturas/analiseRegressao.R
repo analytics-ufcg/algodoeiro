eaabd6d845f715191aa79a34f7cb7d069e14db79
talitalobo <talitabac@gmail.com>
2014-07-28 11:58:45 -0300

    analise de regressao

--- src/R/US 9 - Relação entre algodao e culturas/analiseRegressao.R
+++ src/R/US 9 - Relação entre algodao e culturas/analiseRegressao.R
@@ -1,27 +1,89 @@
-
 # analise de regressao
 
 library(GGally)
+library(RODBC)
 library(ggplot2)
+library(reshape)
+library(car)
 
+channel <- odbcConnect("AlgodoeiroDSN")
 
-# Roteiro
+agricultor_producao = sqlQuery(channel, 
+                               "SELECT a.id, a.nome_agricultor, year(p.data_plantio),
+                               p.quantidade_produzida, p.area_plantada, p.id_cultura, c.nome_cultura from agricultor a, Cultura c,
+                               producao p where p.id_agricultor=a.id and p.id_cultura=c.id order by a.nome_agricultor", stringsAsFactor = FALSE)
 
-colnames(producao_por_cultura)[2] <- "Algodao"
-colnames(producao_por_cultura)[12] <- "MilhoVerde"
-colnames(producao_por_cultura)[15] <- "SorgoForragem"
+agricultor_producao_total <- agricultor_producao
+
+  # apenas com o ano de 2011
+producao_2011 <- subset(agricultor_producao, agricultor_producao$year == 2011)
+
+producao_2011_cul <- melt(producao_2011)
+producao_2011_cul <- subset(producao_2011_cul, variable=="quantidade_produzida")
+producao_2011_cul <- cast(producao_2011_cul, nome_agricultor ~ nome_cultura)
+
+colnames(producao_2011_cul)[2] <- "Algodao"
+colnames(producao_2011_cul)[14] <- "SorgoForragem"
 drops <- c( "Pluma","Caroço", "Guandu", "Fava", "Pepino", "MilhoVerde")
-pairs_producao <- producao_por_cultura[,!(names(producao_por_cultura) %in% drops)]
+pairs_producao <- producao_2011_cul[,!(names(producao_2011_cul) %in% drops)]
 
-ggpairs(data=pairs_producao, # data.frame with variables
-        upper=list(params=list(corSize=9)), axisLabels='show',
-        lower=list(continuous="smooth", params=c(colour="blue")),
-        columns=2:9, # columns to plot, default to all.
-        title="Pairs", # title of the plot
-        na.rm = TRUE)
+# normalidade
+
+  par(mfrow=c(4,4))
+
+  # aplicando a funcao logaritmica, vemos que os dados tendem a seguir uma distribuiçao normal
+
+  hist(log(producao_2011_cul$Algodao))
+  hist(log(producao_2011_cul$Amendoim))
+  hist(log(producao_2011_cul$Caroço))
+  hist(log(producao_2011_cul$Fava))
+  hist(log(producao_2011_cul$Feijão))
+  hist(log(producao_2011_cul$Gergelim))
+  hist(log(producao_2011_cul$Guandu))
+  hist(log(producao_2011_cul$Jerimum))
+  hist(log(producao_2011_cul$Melancia))
+  hist(log(producao_2011_cul$Milho))
+  hist(log(producao_2011_cul$Pepino))
+  hist(log(producao_2011_cul$Pluma))
+  hist(log(producao_2011_cul$SorgoForragem))
+
+#Retirar as culturas abaixo por serem redundantes ou amostragem pequena 
+drops <- c("Pluma","Caroço", "Guandu", "Fava", "Pepino")
+producao_2011_cul <- producao_2011_cul[,!(names(producao_2011_cul) %in% drops)]
 
 
-pairs(data=pairs_producao,
-      lower.panel=panel.smooth,
-      upper.panel=panel.cor,
-      na.rm=TRUE)
+# metrics
+
+  ggpairs(data=log(producao_2011_cul[2:9]), 
+          upper=list(params=list(corSize=9)), axisLabels='show',
+          lower=list(continuous="smooth", params=c(colour="blue")),
+          na.rm = TRUE)
+
+  
+  
+  
+  # analise de regressao
+  dat <- log(producao_2011_cul[2:9])  
+  par(mfrow=c(1,1))
+
+  library(MASS)
+
+  modelo <- lm(dat$Algodao ~ dat$Amendoim + dat$Feijão + dat$Gergelim + dat$Jerimum, data=dat)
+  summary(modelo)   
+  step <- stepAIC(modelo, direction="both")
+  step$anova
+  summary(modelo)
+
+  plot(modelo$fitted.values)
+  plot(modelo$residuals)
+
+
+
+
+
+
+
+
+
+
+
