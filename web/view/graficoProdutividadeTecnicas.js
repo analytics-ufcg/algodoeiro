168874cc4b09bf36aaed647e439a9a1770530d89
Orion <orion.lima@ccc.ufcg.edu.br>
2014-08-04 16:03:00 -0300

    excluindo arquivos que nao sao usados

--- web/view/graficoProdutividadeTecnicas.js
+++ web/view/graficoProdutividadeTecnicas.js
@@ -87,27 +87,4 @@ function graficoProdutividadeTecnicas(div_selector, agricultor, data, regioes) {
         };
     }
 
-    /*// Resolve collisions between nodes.
-    function collide(alpha) {
-        var quadtree = d3.geom.quadtree(data);
-        return function(d) {
-            var r = d.radius + radius + padding, nx1 = d.x - r, nx2 = d.x + r, ny1 = d.y - r, ny2 = d.y + r;
-            quadtree.visit(function(quad, x1, y1, x2, y2) {
-                if (quad.point && (quad.point !== d)) {
-                    var x = d.x - quad.point.x, y = d.y - quad.point.y,
-                        l = Math.sqrt(x * x + y * y),
-                        r = d.radius + quad.point.radius + (d.color !== quad.point.color) * padding;
-                    if (l < r) {
-                        l = (l - r) / l * alpha;
-                        d.x -= x *= l;
-                        d.y -= y *= l;
-                        quad.point.x += x;
-                        quad.point.y += y;
-                    }
-                }
-                return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-            });
-        };
-    }*/
-
 }
