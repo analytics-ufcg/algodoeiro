901cc93ae281d82a9bd6fe92b50830eb1b068fe8
Joao P. Targino <joaotargino@gmail.com>
2014-08-06 15:29:25 -0300

    shape tabela de tecnicas para agrupar

--- src/R/US 10 - Combinação de Técnicas/analiseTecnicas.R
+++ src/R/US 10 - Combinação de Técnicas/analiseTecnicas.R
@@ -25,37 +25,43 @@ temp = producao_tecnicas
 temp$quantidade_produzida[] <- temp$quantidade_produzida/(temp$area_plantada*0.5)
 producao_tecnicas$quantidade_produzida <- temp$quantidade_produzida
 rm(temp)
+colnames(producao_tecnicas)[2] <- "produtividade"
 
 ##Calcula a correlação dos anos de 2010 e 2011 juntos
-agricultor_producao_total <- producao_tecnicas
-agricultor_producao_total$nome_agricultor <- paste(agricultor_producao_total$nome_agricultor , agricultor_producao_total$year, sep=" ")
-agricultor_producao_total <- melt(agricultor_producao_total)
+tabela_tecnicas <- producao_tecnicas
+tabela_tecnicas$nome_agricultor <- paste(tabela_tecnicas$nome_agricultor , tabela_tecnicas$year, sep=" ")
+tabela_tecnicas <- melt(tabela_tecnicas)
 
-agricultor_producao_total <- subset(agricultor_producao_total, variable=="quantidade_produzida")
-agricultor_producao_total <- cast(agricultor_producao_total, nome_agricultor ~ nome_tecnica)
+tabela_tecnicas <- subset(tabela_tecnicas, variable=="produtividade")
+tabela_tecnicas <- cast(tabela_tecnicas, nome_agricultor ~ nome_tecnica)
+tabela_tecnicas[2:17][!is.na(tabela_tecnicas[2:17])] <- 1
+tabela_tecnicas[is.na(tabela_tecnicas)] <- 0
+
+
+write.csv(file="tabela_tecnicas.csv", tabela_tecnicas)
 
 par(mfrow=c(4,4))
 
 # reajustar esse df
-hist(log(agricultor_producao_total[,2]))
-hist(log(agricultor_producao_total[,3]))
-hist(log(agricultor_producao_total[,4]))
-hist(log(agricultor_producao_total[,5]))
-hist(log(agricultor_producao_total[,6]))
-hist(log(agricultor_producao_total[,7]))
-hist(log(agricultor_producao_total[,8]))
-hist(log(agricultor_producao_total[,9]))
-hist(log(agricultor_producao_total[,10]))
-hist(log(agricultor_producao_total[,11]))
-hist(log(agricultor_producao_total[,12]))
-hist(log(agricultor_producao_total[,13]))
-hist(log(agricultor_producao_total[,14]))
-hist(log(agricultor_producao_total[,15]))
-hist(log(agricultor_producao_total[,16]))
-hist(log(agricultor_producao_total[,17]))
+hist(log(tabela_tecnicas[,2]))
+hist(log(tabela_tecnicas[,3]))
+hist(log(tabela_tecnicas[,4]))
+hist(log(tabela_tecnicas[,5]))
+hist(log(tabela_tecnicas[,6]))
+hist(log(tabela_tecnicas[,7]))
+hist(log(tabela_tecnicas[,8]))
+hist(log(tabela_tecnicas[,9]))
+hist(log(tabela_tecnicas[,10]))
+hist(log(tabela_tecnicas[,11]))
+hist(log(tabela_tecnicas[,12]))
+hist(log(tabela_tecnicas[,13]))
+hist(log(tabela_tecnicas[,14]))
+hist(log(tabela_tecnicas[,15]))
+hist(log(tabela_tecnicas[,16]))
+hist(log(tabela_tecnicas[,17]))
 
 
-#ggpairs(data=agricultor_producao_total[2:17], 
+#ggpairs(data=tabela_tecnicas[2:17], 
 #        upper=list(params=list(corSize=17)), axisLabels='show',
 #       lower=list(continuous="smooth", params=c(colour="blue")),
 #        na.rm = TRUE)
