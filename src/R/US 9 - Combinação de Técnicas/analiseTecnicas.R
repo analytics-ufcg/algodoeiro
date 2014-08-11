fba8173d4e044723366f9f554d491c28edc6ec83
talitalobo <talitabac@gmail.com>
2014-08-11 15:40:56 -0300

    correção dos dataframes. proximo passo: plotar o gráfico

--- src/R/US 9 - Combinação de Técnicas/analiseTecnicas.R
+++ src/R/US 9 - Combinação de Técnicas/analiseTecnicas.R
@@ -13,16 +13,18 @@ producao_tecnicas = sqlQuery(channel, "SELECT distinct a.nome_agricultor, p.quan
                                where p.id_agricultor = a.id and p.id_cultura = c.id and ta.id_agricultor = a.id
                                and c.id=1 and t.id = ta.id_tecnica  order by a.nome_agricultor", stringsAsFactor = FALSE)
 
+close(channel)
+
 write.csv(file="producao_tecnicas.csv", producao_tecnicas)
 
-# correçao para produtividade
+# Correçao para produtividade (produtividade = producao/(area)*0.5)
 temp = producao_tecnicas
 temp$quantidade_produzida[] <- temp$quantidade_produzida/(temp$area_plantada*0.5)
 producao_tecnicas$quantidade_produzida <- temp$quantidade_produzida
 rm(temp)
 colnames(producao_tecnicas)[2] <- "produtividade"
 
-##Calcula a correlação dos anos de 2010 e 2011 juntos
+# Correcao do data frame, 0 pra quem nao usa a tecnica, 1 pra quem usa
 tabela_tecnicas <- producao_tecnicas
 tabela_tecnicas$nome_agricultor <- paste(tabela_tecnicas$nome_agricultor , tabela_tecnicas$year, sep=" ")
 tabela_tecnicas <- melt(tabela_tecnicas)
@@ -32,34 +34,45 @@ tabela_tecnicas <- cast(tabela_tecnicas, nome_agricultor ~ nome_tecnica)
 tabela_tecnicas[2:17][!is.na(tabela_tecnicas[2:17])] <- 1
 tabela_tecnicas[is.na(tabela_tecnicas)] <- 0
 
+# --- correcao do data frame
+colnames(tabela_tecnicas)[2] <- "AdubacaoOrganica"
+colnames(tabela_tecnicas)[3] <- "AplicacaoDeCaulim"
+colnames(tabela_tecnicas)[4] <- "AplicacaoDeNim"
+colnames(tabela_tecnicas)[5] <- "AplicacaoDeUrinaDeVaca"
+colnames(tabela_tecnicas)[7] <- "CoberturaMorta"
+colnames(tabela_tecnicas)[8] <- "CortandoAsAguas"
+colnames(tabela_tecnicas)[9] <- "CurvasDeNivel"
+colnames(tabela_tecnicas)[11] <- "EnleiramentoDosGarranchos"
+colnames(tabela_tecnicas)[12] <- "OutrosBioprotetoresNaturais"
+colnames(tabela_tecnicas)[13] <- "PlantioEmNivel"
+colnames(tabela_tecnicas)[14] <- "PreparoDoSoloComTraçãoAnimal"
+colnames(tabela_tecnicas)[15] <- "PreparoDoSoloComTratorEGrade"
+colnames(tabela_tecnicas)[16] <- "QuebraVento"
 
 write.csv(file="tabela_tecnicas.csv", tabela_tecnicas)
 
-par(mfrow=c(4,4))
+par(mfrow=c(1,1))
 
-# reajustar esse df
-hist(log(tabela_tecnicas[,2]))
-hist(log(tabela_tecnicas[,3]))
-hist(log(tabela_tecnicas[,4]))
-hist(log(tabela_tecnicas[,5]))
-hist(log(tabela_tecnicas[,6]))
-hist(log(tabela_tecnicas[,7]))
-hist(log(tabela_tecnicas[,8]))
-hist(log(tabela_tecnicas[,9]))
-hist(log(tabela_tecnicas[,10]))
-hist(log(tabela_tecnicas[,11]))
-hist(log(tabela_tecnicas[,12]))
-hist(log(tabela_tecnicas[,13]))
-hist(log(tabela_tecnicas[,14]))
-hist(log(tabela_tecnicas[,15]))
-hist(log(tabela_tecnicas[,16]))
-hist(log(tabela_tecnicas[,17]))
+
+# Passos a seguir
+# --- plotar o gráfico de combinacao de tecnicas x receita
+# --- correlacao e analise
+# --- refletir quais modelos seguir
+# --- gerar modelos de regressao
+
+
+# falta adicionar a produtividade de algodao no df das tecnicas
+
+
+
+
+
+
+
+
 
 
 #ggpairs(data=tabela_tecnicas[2:17], 
 #        upper=list(params=list(corSize=17)), axisLabels='show',
 #       lower=list(continuous="smooth", params=c(colour="blue")),
 #        na.rm = TRUE)
-
-
-close(channel)
