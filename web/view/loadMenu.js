ed7f318605eae1f7f7a3b71bc80cf6676d1143c4
Rafaelle Amorim <rafaelle@kendall.(none)>
2014-08-15 11:58:20 -0300

    Ajuste na interface para celular

--- web/view/loadMenu.js
+++ web/view/loadMenu.js
@@ -1,13 +1,48 @@
 var menuAtivo = null;
 
 $( document ).ready( function() {
-	$("#nav-phone").html($("#nav-desktop").html());
-	$("#nav-phone").on("click", "li", function () {
-		if (menuAtivo !== null) {
-			menuAtivo.removeClass("active");
-		}
+	$("#navbar-phone-options").html($("#navbar-desktop-options").html());
+
+	$(".navbar-custom-ul").on("click", "li", function () {
+		desativarMenuAtivo();
+		esconderMenu();
 
 		menuAtivo = $(this);
 		menuAtivo.addClass("active");
 	});
+
+	$("#navbar-phone-home").click(function () {
+		desativarMenuAtivo();
+		esconderMenu();
+	});
+
+	function desativarMenuAtivo() {
+		if (menuAtivo !== null) {
+			menuAtivo.removeClass("active");
+		}
+	}
+
+	function esconderMenu() {
+		var bootstrapEnvironment = findBootstrapEnvironment();
+		if (bootstrapEnvironment === 'xs') {
+			$("#navbar-phone-toggle").click();
+		}
+	}
+
+	function findBootstrapEnvironment() {
+	    var envs = ['xs', 'sm', 'md', 'lg'];
+
+	    $el = $('<div>');
+	    $el.appendTo($('body'));
+
+	    for (var i = envs.length - 1; i >= 0; i--) {
+	        var env = envs[i];
+
+	        $el.addClass('hidden-'+env);
+	        if ($el.is(':hidden')) {
+	            $el.remove();
+	            return env
+	        }
+    };
+}
 });
