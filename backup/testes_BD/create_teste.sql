a92a599f94d122595477d9fc13797f885579e3bb
Celio Roberto Cavalcante de Barros Filho <celio@pearson.(none)>
2014-09-02 17:32:37 -0300

    testando update do agricultor do bd com os parametros que existem no view

---
+++ backup/testes_BD/create_teste.sql
@@ -0,0 +1,1 @@
+CREATE TABLE Agricultor2(id auto_increment, nome_agricultor VARCHAR(50), sexo VARCHAR(1), ano_adesao INT, PRIMARY KEY (id));
---
+++ backup/testes_BD/testandoBD.py
@@ -0,0 +1,43 @@
+import pyodbc
+import time
+
+def create_connection():
+    return pyodbc.connect("DSN=AlgodoeiroDSN")
+
+
+def insert_Agricultor():
+    cnxn = create_connection()
+    cursor = cnxn.cursor()
+    
+#    cursor.execute("SELECT nome_agricultor, sexo, ano_adesao FROM Agricultor")
+#    rows = cursor.fetchall()
+    
+    try:
+        cursor.execute("INSERT INTO Agricultor2(nome_agricultor,sexo,ano_adesao) VALUES (?,?,?);", "JOAO PAULO", "M", 2014)
+        cursor.commit()
+    except:
+        # Rollback in case there is any error
+       print "ERRO"
+       cursor.rollback()
+
+    cnxn.close()
+
+def update_Agricultor(id, nome, sexo, ano_adesao, variedade_algodao, comunidade):
+    cnxn = create_connection()
+    cursor = cnxn.cursor()
+    try:
+       cursor.execute("SELECT id from Comunidade where nome_comunidade=?",comunidade)
+       id_comunidade = cursor.fetchall()
+       cursor.execute("UPDATE Agricultor SET nome_agricultor= ?, sexo=?, ano_adesao=?, variedade_algodao=?, id_comunidade=? WHERE id=?", nome, sexo, ano_adesao, variedade_algodao,id_comunidade[0][0], id)
+       cursor.commit()
+    except:
+       print "ERRO"
+       cursor.rollback()
+    cnxn.close()
+
+
+if __name__ == '__main__':
+    ini = time.time()
+    #update_Agricultor(4, "Ademar Alves", "M", 2010, "Aroeira", "P.A dos Dez")
+    fim = time.time()
+    print "Funcao soma2: ", fim-ini
