c53783f1f1d47141468bd81c2d5da647099a7c2f
Andryw Marques Ramos <andryw@teste-t.(none)>
2014-09-15 15:34:05 -0300

    algumas modificacoes em producao

--- src/apiRest/dadosApiRestInsercao.py
+++ src/apiRest/dadosApiRestInsercao.py
@@ -15,7 +15,7 @@ def producoes_e(id_agricultor,ano):
     cnxn = create_connection()
     cursor = cnxn.cursor()
     cursor.execute("SELECT id_agricultor, nome_agricultor, id_producao, quantidade_produzida, data_plantio,"
-                   " id_cultura, nome_cultura FROM cultura c LEFT OUTER JOIN "
+                   " c.id as id_cultura, nome_cultura FROM cultura c LEFT OUTER JOIN "
                    "(select a.id as id_agricultor, a.nome_agricultor as nome_agricultor, p.id as id_producao, p.id_cultura as id_cultura, "
                    "p.data_plantio as data_plantio, p.quantidade_produzida as quantidade_produzida "
                    " from agricultor a, producao p where a.id = p.id_agricultor and a.id = %d and year(p.data_plantio) = %d) p "
