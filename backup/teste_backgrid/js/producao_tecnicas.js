fee020d38ee93c8a4edc3e434672eb93fcb87a13
Fábio Silva <fabio.fernando.osilva@gmail.com>
2014-09-17 22:42:10 -0300

    Visão de Produção e Técnicas

--- backup/teste_backgrid/js/producao_tecnicas.js
+++ backup/teste_backgrid/js/producao_tecnicas.js
@@ -24,115 +24,58 @@ function readJSON(url){
 }
 
 $(document).ready(function() {
-
 	function getRegiaoSelecionada() {
-		return $("#dropdown option:selected").val();
+		return $("#dropdownRegiao option:selected").val();
 	}
 
-	var DeleteCell = Backgrid.Cell.extend({
-	    template: _.template("<a href=\"#\"><span class=\"glyphicon glyphicon-trash\"></span></a>"),
-	    events: {
-	      "click": "deleteRow"
-	    },
-	    deleteRow: function (e) {
-	      	e.preventDefault();
-	      	var agricultor = this.model;
-	      	if ($('#remove_agricultor_' + this.model.id).length < 1) {
-	      		$("body").append('<button id="remove_agricultor_' + this.model.id + '" hidden="hidden">OK</button>');
-
-	      		$('#remove_agricultor_' + this.model.id).confirmModal({
-		    		confirmTitle : 'Confirma remoção',
-		    		confirmMessage : 'Realmente você deseja remover esse agricultor?',
-		    		confirmOk : 'Confirma',
-		    		confirmCancel : 'Cancela',
-		    		confirmDirection : 'rtl',
-		    		confirmStyle : 'primary',
-					confirmCallback : function (){
-						$.ajax({
-							type: 'post',
-							contentType: "application/json; charset=utf-8",
-							scriptCharset: "utf-8" ,
-							url: REST_SERVER + '/removeAgricultor/' + getRegiaoSelecionada(),
-							data: JSON.stringify(agricultor),
-							dataType: 'json',
-							success: function(){
-							   atualizar_regiao(getRegiaoSelecionada());
-							},
-							error: function(){
-							   alert('failure');
-							}
-						});
-					}
-    			});
-	      	}
-
-    		$('#remove_agricultor_' + this.model.id).trigger( "click" );
-	    },
-	    render: function () {
-	      this.$el.html(this.template());
-	      this.delegateEvents();
-	      return this;
-	    }
-	});
+	function getAnoSelecionado() {
+		return $("#dropdownAno option:selected").val();
+	}
 
 	var regiao = readJSON(REST_SERVER + "/regioes");
-
 	$.each(regiao, function(index, value) {
-	     $('#dropdown').append($('<option>').text(value.regiao).attr('value', value.id));
+	     $('#dropdownRegiao').append($('<option>').text(value.regiao).attr('value', value.id));
+	});
+
+	var ano = readJSON(REST_SERVER + "/lista_ano_e");
+	$.each(ano.ano_atividade, function(index, value) {
+	     $('#dropdownAno').append($('<option>').text(value[0]).attr('value', value[1]));
 	});
 
 	var regiao_selecionada = getRegiaoSelecionada();
+	var ano_selecionado = getAnoSelecionado();
 
-	$('#dropdown').change(function(data) {
+	$('#dropdownRegiao').change(function(data) {
 		regiao_selecionada = getRegiaoSelecionada();
-		atualizar_regiao(regiao_selecionada);
+		atualizar_grid(regiao_selecionada, ano_selecionado);
 	});
 
-	var Agricultor = Backbone.Model.extend({
-	  initialize: function () {
-	    Backbone.Model.prototype.initialize.apply(this, arguments);
-	    this.on("change", function (model, options) {
-		   	var newModel = model.toJSON();
-
-		    if (options && options.save === false) return;
-		    model.save(newModel, {
-		       	error: function() { 
-		       		alert("Não foi possível realizar a alteração.");
-		      		atualizar_regiao(getRegiaoSelecionada());
-
-		        },
-		        success: function() {
-		        },
-		        wait: true
-	        });
-
-	        
-		  });
-	    }
+	$('#dropdownAno').change(function(data) {
+		ano_selecionado = getAnoSelecionado();
+		atualizar_grid(regiao_selecionada, ano_selecionado);
 	});
 
-	atualizar_regiao(getRegiaoSelecionada());
+	var ProducaoTecnicas = Backbone.Model.extend({});
 
-	function atualizar_regiao(regiao_selecionada) {
-		var Agricultores = Backbone.Collection.extend({
-			model : Agricultor,
+	atualizar_grid(regiao_selecionada, ano_selecionado);
+
+	function atualizar_grid(regiao_selecionada, ano_selecionado) {
+		var ProducoesTecnicas = Backbone.Collection.extend({
+			model : ProducaoTecnicas,
 			//url : "http://analytics.lsd.ufcg.edu.br/algodoeiro_rest/agricultor_e"
-			url : REST_SERVER + "/agricultor_e/" + regiao_selecionada
+			url : REST_SERVER + "/producao_tecnica_agricultor/" + regiao_selecionada + "/" + ano_selecionado
 		});
 
-		var agricultores = new Agricultores();
-		agricultores.fetch({
+		var producoesTecnicas = new ProducoesTecnicas();
+		
+		producoesTecnicas.fetch({
 			reset : true
 		});
 
 		var comunidade = readJSON(REST_SERVER + "/comunidades_e/" + regiao_selecionada);
 		
-		atualizar_regiao_form(regiao_selecionada);
-		resetarForm();
-
-		var columns = [{
-			cell: DeleteCell
-		}, {
+		var columns = [
+		{
 			name : "id", 
 			label : "Id", 
 			editable : false, 
@@ -141,181 +84,28 @@ $(document).ready(function() {
 			}),
 			renderable: false
 		}, {
-			name : "nome_agricultor",
+			name : "nome",
 			label : "Nome",
 			cell : "string"
+		},  {
+			name : "teve_tecnicas",
+			label : "Técnicas",
+			cell : "boolean"
 		}, {
-			name : "sexo",
-			label : "Sexo",
-			cell: Backgrid.SelectCell.extend({
-		      // It's possible to render an option group or use a
-		      // function to provide option values too.
-		      optionValues: [["Masculino", "M"], ["Feminino", "F"]]
-		    })
-		}, {
-			name : "ano_adesao",
-			label : "Ano Adesão",
-			cell : "string" 
-		}, {
-			name : "variedade_algodao",
-			label : "Variedade Algodão",
-			cell : "string"
-		}, {
-			name : "id_comunidade",
-			label : "Comunidade",
-			cell : Backgrid.SelectCell.extend({
-		      // It's possible to render an option group or use a
-		      // function to provide option values too.
-		      optionValues: comunidade["comunidade"]
-		    })
+			name : "teve_producao",
+			label : "Produção",
+			cell : "boolean"
 		}];
 
 		grid = new Backgrid.Grid({
 			columns : columns,
-			collection : agricultores
+			collection : producoesTecnicas
 		});
 
-		grid.render().sort("nome_agricultor", "ascending");
+		grid.render().sort("nome", "ascending");
 
-		$("#tabela_agricultores").empty();
-		$("#tabela_agricultores").append(grid.el);
+		$("#tabela_producoes_tecnicas").empty();
+		$("#tabela_producoes_tecnicas").append(grid.el);
 
 	}
-	
-    $("#limpar_campos").click(function () {
-    	$("#novo_agricultor_form").data('bootstrapValidator').resetForm();
-		atualizar_regiao_form(getRegiaoSelecionada());
-    });
-
-	function resetarForm() {
-        $("#limpar_campos").trigger( "click" );
-	}
-
-	function atualizar_regiao_form(regiao_selecionada) {
-		var comunidades = readJSON(REST_SERVER + "/comunidades_e/" + regiao_selecionada);
-
-		$('select[name="comunidade"]').html('');
-
-		$.each(comunidades.comunidade, function(index, value) {
-		    $('select[name="comunidade"]').append($('<option>').text(value[0]).attr('value', value[1]));
-		});
-	}
-
-	// Render the grid and attach the root to your HTML document
-	//$("#example-1-result").append(grid.render().el);
-
-	// Fetch some countries from the url
-	//territories.fetch({reset: true});
-	// var nova_linha;
-
-	// $("#acrescentar").click(function() {
-	// 	nova_linha = grid.insertRow({nova_linha: "true"});
-	// });
-	$('#novo_agricultor_form').bootstrapValidator({
-        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-            nome_agricultor: {
-                validators: {
-                    notEmpty: {},
-                    stringLength: {
-                        min: 3,
-                        max: 250
-                    },
-                    regexp: {
-                        regexp: /^[a-zA-Zà-úÀ-Ú ]+$/
-                    }
-                }
-            },
-            ano_adesao: {
-                validators: {
-                }
-            },
-            sexo: {
-                validators: {
-                    notEmpty: {}
-                }
-            },
-            variedade_algodao: {
-                validators: {
-                    notEmpty: {},
-                    stringLength: {
-                        min: 3,
-                        max: 50
-                    },
-                    regexp: {
-                        regexp: /^[a-zA-Zà-úÀ-Ú ]+$/
-                    }
-                }
-            }
-        }
-    })
-    .on('success.form.bv', function(e) {
-    	function ConvertFormToJSON(form){
-		    var array = jQuery(form).serializeArray();
-		    var json = {};
-		    
-		    jQuery.each(array, function() {
-		        json[this.name] = this.value || '';
-		    });
-		    
-		    return json;
-		}
-
-        e.preventDefault();
-        var nome_agricultor_val = $('input[name="nome_agricultor"]').val();
-        var sexo_val = $('input[name="sexo"]:checked').val();
-        var ano_adesao_val = $('input[name="ano_adesao"]').val();
-        var regiao_val = getRegiaoSelecionada();
-        var comunidade_val = $('select[name="comunidade"] option:selected').val();
-        var variedade_algodao_val = $('input[name="variedade_algodao"]').val();
-
-        // grid.insertRow({nome_agricultor: nome_agricultor_val, sexo: sexo_val, ano_adesao: ano_adesao_val, variedade_algodao: variedade_algodao_val, id_comunidade: comunidade_val, regiao: regiao_val});
-
-
-        // Get the form instance
-        var $form = $(e.target);
-
-        // Get the BootstrapValidator instance
-        var bv = $form.data('bootstrapValidator');
-        var send_data = ConvertFormToJSON($form);
-        // Use Ajax to submit form data
-
-		$.ajax({
-			type: 'post',
-			contentType: "application/json; charset=utf-8",
-			scriptCharset: "utf-8" ,
-			url: REST_SERVER + '/adicionaAgricultor/' + regiao_val,
-			data: JSON.stringify(send_data),
-			dataType: 'json',
-			success: function(){
-			   if (regiao_selecionada == regiao_val) {
-			       atualizar_regiao(regiao_val);
-			   }
-
-	           resetarForm();
-			},
-			error: function(){
-			   alert('failure');
-			}
-		});
-
-//          $.post(, {nome_agricultor: nome_agricultor_val, sexo: sexo_val, ano_adesao: ano_adesao_val, variedade_algodao: variedade_algodao_val, id_comunidade: comunidade_val, regiao: regiao_val}, function(result) {
-//              alert( "success" );
-//          }, 'json')
-//          .done(function() {
-		// 	alert( "second success" );
-		// })
-		// .fail(function() {
-		// alert( "error" );
-		// })
-		// .always(function() {
-		// alert( "finished" );
-		// });
-    });
-
 });
