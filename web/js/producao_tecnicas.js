a1b3996d242cbd1344f0cdbbf346b3c987d4d67c
Fábio Silva <fabio.fernando.osilva@gmail.com>
2014-09-24 11:32:05 -0300

    Refatoramento - Removendo duplicidade de código

--- web/js/producao_tecnicas.js
+++ web/js/producao_tecnicas.js
@@ -3,52 +3,6 @@ var grid;
 var REST_SERVER = 'http://analytics.lsd.ufcg.edu.br/algodoeiro_rest';
 var EDIT_CELL = 'editProdTec.html';
 
-function parseURLParams(url) {
-    var queryStart = url.indexOf("?") + 1,
-        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
-        query = url.slice(queryStart, queryEnd - 1),
-        pairs = query.replace(/\+/g, " ").split("&"),
-        parms = {}, i, n, v, nv;
-
-    if (query === url || query === "") {
-        return;
-    }
-
-    for (i = 0; i < pairs.length; i++) {
-        nv = pairs[i].split("=");
-        n = decodeURIComponent(nv[0]);
-        v = decodeURIComponent(nv[1]);
-
-        if (!parms.hasOwnProperty(n)) {
-            parms[n] = [];
-        }
-
-        parms[n].push(nv.length === 2 ? v : null);
-    }
-    return parms;
-}
-
-function readJSON(url){
-	var dataframe;
-
-	$.ajax({
-        url : url,
-        type : 'GET',
-        async: false,
-        dataType : 'json',
-        success: function(data) { 
-        	console.log("success ajax!");
-        	dataframe = data;
-         },                                                                                                                                                                                       
-       error: function(xhr, status, error) {
-          var err = eval("(" + xhr.responseText + ")");
-          console.log(err.Message);
-        }
-    });
-
-	return dataframe;
-}
-
 $(document).ready(function() {
 	var EditCell = Backgrid.Cell.extend({
 	    template: _.template("<a href=\"#\"><span class=\"glyphicon glyphicon-pencil\"></span></a>"),
@@ -146,7 +100,6 @@ $(document).ready(function() {
 	function atualizar_grid(regiao_selecionada, ano_selecionado) {
 		var ProducoesTecnicas = Backbone.Collection.extend({
 			model : ProducaoTecnicas,
-			//url : "http://analytics.lsd.ufcg.edu.br/algodoeiro_rest/agricultor_e"
 			url : REST_SERVER + "/producao_tecnica_agricultor/" + regiao_selecionada + "/" + ano_selecionado
 		});
 
